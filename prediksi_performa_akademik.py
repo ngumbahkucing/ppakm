3749d3286dc0a3b489547f1173cf20e1a9e486b4
Taukhid Wardoyo <taukhid.wardoyo@gmail.com>
2024-02-03 11:06:10 +0700

    Update prediksi_performa_akademik.py

--- prediksi_performa_akademik.py
+++ prediksi_performa_akademik.py
@@ -7,6 +7,7 @@ from streamlit_option_menu import option_menu
 
 
 from PIL import Image
+import sklearn
 
 image = Image.open('Bobot fitur.jpg')
 
@@ -14,119 +15,90 @@ image = Image.open('Bobot fitur.jpg')
 
 
 def main():
-    
-    choice = option_menu(None, ["Home","Prediksi","Hasil","Penilaian","Tentang"], 
+    print(sklearn.__version__)
+    choice = option_menu(None, ["Beranda","Prediksi","Hasil","Penilaian","Tentang"], 
     icons=['house','trophy', 'camera', 'list-task', 'book'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
     st.header('Prediksi Performa Akademik Mahasiswa')
-
-    if choice == "Home":
+    st.write('Oleh Sri Handayani') 
+        
+    if choice == "Beranda":
         st.subheader("Definisi dan penjelasan")
-        st.write('Performa akademik mahasiswa adalah capaian seorang mahasiswa dalam mendapatkan nilai akademik, indikator performa akademik diperoleh dari nilai Indek Prestasi Sementara (IPS) maupun Indek Prestasi Komulatif (IPK).') 
+        st.write('Kinerja akademis mahasiswa mengacu pada pencapaian seorang mahasiswa dalam aspek nilai akademik, yang diukur melalui Indeks Prestasi Semester (IPS) dan Indeks Prestasi Kumulatif (IPK).') 
         st.write('Banyak variabel yang mempengaruhi performa akademik mahasiswa baik variabel perilaku didalam pembelajaran maupun variabel lainnya. Pada sistem informasi prediksi performa akademik mahasiswa ini variabel yang digunakan adalah variabel akademik dan non-akademik.') 
-        st.write('Variabel akademik adalah variabel yang diperoleh dari kegiatan akademik yaitu variabel IPK/IPS dan perilaku mahasiswa dalam berinteraksi dengan Learning Management System (LMS). Sedangkan variabel non-akademik terdiri dari faktor ekonomi, domisili, gender dan keikutsertaan mahasiswa dalam berorganisasi kampus.')
-        st.write('Prediksi performa akademik mahasiswa merupakan aktivitas yang sangat penting, hal ini dikarenakan dengan memprediksi performa akademik mahasiswa, dapat menciptakan peluang untuk meningkatkan hasil pendidikan. Selain itu dengan pendekatan prediksi performa yang efektif, instruktur dapat mengalokasikan sumber daya dan instruksi yang lebih akurat. Prediksi awal performa mahasiswa dapat membantu pengambil keputusan untuk memberikan tindakan pada saat yang tepat dan untuk merencanakan pembelajaran yang tepat dalam meningkatkan tingkat keberhasilan mahasiswa')
-        st.write('Performa akademik mahasiswa perlu dipantau dengan cermat untuk membantu lembaga mengidentifikasi mahasiswa yang berisiko gagal, mencegah mereka DO atau lulus terlambat (Nachouki dan Abou Naaj 2022). Performa akademik yang buruk menjadi indikator mahasiswa kesulitan dalam menyesuaikan diri dengan perguruan tinggi dan berpeluang besar untuk putus dekolah/drop out (DO) (Lau 2003). Beberapa dampak negatif dari buruknya performa akademik dan tingginya DO diantaranya yaitu rendahnya tingkat kelulusan tepat waktu (Delen 2010), membuang biaya Pendidikan yang sia-sia (Costa, Bispo, dan Pereira 2018), pengurangan kesempatan hidup secara profesional dan sosial yang berdampak negatif kepada keluarga dan masyarakat (Casanova dkk. 2021), sulitnya perguruan tinggi dalam mendapatkan akreditasi yang baik (Delen, Topuz, dan Eryarsoy 2020), hilangnya kepercayaan dan motivasi untuk melanjutkan Pendidikan (Coussement dkk. 2020), dan lain sebagainya.')    
-        st.write('Sistem informasi Prediksi Performa Akademik Mahasiswa ini sangat penting bagi Mahasiswa, Dosen, Staff Akademik dan lainnya. Berdasarkan data yang digunakan untuk membangun model, maka sistem informasi prediksi performa akademik mahasiswa ini sangat baik digunakan untuk memprediksi performa akademik mahasiswa semester dua dan empat')
-        st.write('Untuk melakukan prediksi performa akademik mahasiswa, maka silahkan ke menu :blue[Prediksi], Jangan lupa setelah melakukan prediksi silahkan berikan penilaian dan masukan untuk pengembangan aplikasi selanjutnya di menu :blue[Penilaian]')
+        st.write('Analisis prediksi kinerja akademis mahasiswa berperan vital dalam meningkatkan kualitas pendidikan. Melalui prediksi ini, pendidik dapat menyusun strategi dan alokasi sumber daya yang lebih tepat guna. Prediksi yang dilakukan sejak dini memungkinkan pengambil keputusan untuk mengidentifikasi mahasiswa yang membutuhkan bantuan lebih lanjut, sehingga dapat merencanakan program pembelajaran yang meningkatkan peluang sukses mereka.')
+        st.write('Sistem Analisis Prediksi Kinerja Akademis Mahasiswa ini menjadi alat penting bagi Mahasiswa, Dosen, dan Staf Akademik. Dengan berbasis pada data yang digunakan untuk membangun model prediksi, sistem ini sangat efektif untuk memproyeksikan kinerja akademis mahasiswa.')
+        st.write('Untuk memulai analisis prediksi kinerja akademis mahasiswa, silakan kunjungi menu Prediksi. Setelah melakukan prediksi, jangan ragu untuk memberikan feedback dan saran untuk pengembangan sistem lebih lanjut melalui menu Penilaian.')
 
 
     elif choice == "Prediksi":
 
-        st.write('Prediksi performa akademik ini menggunakan dua sumber data yaitu 1). Data Akademik yang berasal dari aktifitas mahasiswa di LMS (Moodle),  2). Data non-akademik (ekonomi, domisili, gender, keikutsertaan mahasiswa dalam berorganisasi kampus). Sistem informasi prediksi ini sangat tepat jika digunakan untuk memprediksi performa akademik mahasiswa semester dua dan empat.')
+        st.write('Prediksi performa akademik ini menggunakan dua sumber data yaitu 1). Data Akademik Mahasiswa IPK Semester dan Data Nilai Ijazah Calon Mahasiswa  2). Data non-akademik (gender, umur, gaji orang tua, jumlah saudara).')
         
         st.subheader("Masukkan Data Diri Anda")
-        nama = st.text_input("Nama")
+        nama = st.text_input("Nama Lengkap")
 
-        colus,colkel = st.columns(2)
-        with colus:
-            status = st.radio(
-                    'Status',
-                    ('Mahasiswa', 'Dosen', 'Staff Akademik', 'Lainnya'))
-        with colkel:
-            jenkel = st.radio(
-                    'Jenis Kelamin',
-                    ('Pria', 'Wanita'))
+        status = st.radio(
+            'Status',
+            ('Mahasiswa', 'Calon Mahasiswa'))
 
 
-        kota_tinggal = st.text_input("Kota Tinggal")
+        st.subheader("Masukkan Data Akademik")
 
-
-        st.subheader("Masukkan Data Akademik (Kegiatan di LMS)")
-        st.write('Perlu dipahami bahwasanya masing-masing fitur memiliki bobot yang berbeda, masing-masing bobot ditunjukkan pada caption fitur')
-
-        col1,col2 = st.columns(2)
-        with col1:
-            Total_login = st.slider('Jumlah Login LMS :blue[(0,120)]', 0, 140)
-            N_access_forum = st.slider('Jumlah Akses Forum LMS :blue[(0,117)]', 0, 3000)
-            N_access_didactic_units = st.slider('Jumlah Akses Materi :blue[(0,077)]', 0, 750)
-            Total_assignments = st.slider('Jumlah Tugas :blue[(0,066)]', 0, 1000)
-            N_assignments_submitted = st.slider('Jumlah Upload Tugas :blue[(0,187)]', 0, 150)
-            N_access_questionnaires = st.slider('Jumlah Membuka Quiz :blue[(0,038)]', 0, 1000)
-            N_attempts_questionnaires = st.slider('Jumlah Melengkapi Quiz :blue[(0,039)]', 0, 1000)
-
-        with col2:
-            N_answered_questions = st.slider('Jumlah Menjawab Quiz :blue[(0,055)]', 0, 120)
-            N_questionnaire_views = st.slider('Jumlah Melihat Quiz :blue[(0,033)]', 0, 1000)
-            N_questionnaires_submitted = st.slider('Jumlah Mengirim Quiz :blue[(0,043)]', 0, 40)
-            N_reviews_questionnaire = st.slider('Jumlah Ulasan Quiz :blue[(0,072)]', 0, 500)
-            Days_first_access_x = st.slider('Minggu Keberapa Akses LMS :blue[(0,014)]', 0, 3)
-            N_entries_course_x = st.slider('Jumlah Masuk Ke Mata Kuliah :blue[(0,057)]', 0, 3000)
-
+        Ipk = st.text_input('IPK Semester Lalu')
+        Semester = st.selectbox('Semester ke-', ('2','3','4'))
+        Prestasi = st.selectbox('Prestasi', ('Tidak Ada','Akademik','Non-Akademik'))
+        if Prestasi == "Tidak Ada":
+            Prestasi = 0;
+        elif Prestasi == "Akademik":
+            Prestasi = 1;
+        else:
+            Prestasi = 2;
+            
 
         st.subheader("Masukkan Data Non Akademik")
 
-        cols,colss = st.columns(2)
-        with cols:
-            Gender = st.radio(
-                    'Jenis Kelamin :blue[(0,025)]',
-                    ('Pria', 'Wanita'))
-            if Gender == 'Pria':
-                Gender = 1
-            else:
-                Gender = 0
+        Gender = st.radio(
+            'Jenis Kelamin',
+            ('Pria', 'Wanita'))
+        if Gender == 'Pria':
+            Gender = 1
+        else:
+            Gender = 0
 
-            Campus_organization = st.slider('Jumlah Organisasi Kampus Yang Diikuti :blue[(0,011)]', 0, 4)
+        Umur = st.text_input("Umur")
 
-        with colss:
-            Economy = st.selectbox(
-                    'Pendapatan Orang Tua :blue[(0,025)]',
-                    ('100.000 - 600.000', '500.000 - 1.000.000', '1.000.000 - 2.500.000', 
-                        '2.500.000 - 5.000.000', '5.000.000 - 7.500.000',
-                        '7.500.000 - 10.000.000', '> 10.000.000')) 
-            if Economy == '100.000 - 600.000':
-                Economy = 1
-            elif Economy == '500.000 - 1.000.000':
-                Economy = 2
-            elif Economy == '1.000.000 - 2.500.000':
-                Economy = 3
-            elif Economy == '2.500.000 - 5.000.000':
-                Economy = 4
-            elif Economy == '5.000.000 - 7.500.000':
-                Economy = 5
-            elif Economy == '7.500.000 - 10.000.000':
-                Economy = 6
-            else:
-                Economy = 7
-
-            Domicile = st.selectbox(
-                    'Jarak Kampus dengan Domisili :blue[(0,022)]',
-                    ('Dalam Kota', 'Kota Sebelah', 'Jarak Satu Kota', 'Dalam Satu Pulau', 'Diluar Pulau'))
-
-            if Domicile == 'Dalam Kota':
-                Domicile = 1
-            elif Domicile == 'Kota Sebelah':
-                Domicile = 2
-            elif Domicile == 'Jarak Satu Kota':
-                Domicile = 3
-            elif Domicile == 'Dalam Satu Pulau':
-                Domicile = 4
-            else : Domicile = 5            
+        Saudara = st.text_input("Jumlah Saudara")
+            
+        Economy = st.selectbox(
+                'Pendapatan Orang Tua',
+                ('100.000 - 600.000', '500.000 - 1.000.000', '1.000.000 - 2.500.000', 
+                    '2.500.000 - 5.000.000', '5.000.000 - 7.500.000',
+                    '7.500.000 - 10.000.000', '> 10.000.000')) 
+        if Economy == '100.000 - 600.000':
+            Economy = 1
+        elif Economy == '500.000 - 1.000.000':
+            Economy = 2
+        elif Economy == '1.000.000 - 2.500.000':
+            Economy = 3
+        elif Economy == '2.500.000 - 5.000.000':
+            Economy = 4
+        elif Economy == '5.000.000 - 7.500.000':
+            Economy = 5
+        elif Economy == '7.500.000 - 10.000.000':
+            Economy = 6
+        else:
+            Economy = 7
 
         with open("prediksi_performa_akademik.sav", "rb") as file:
             model = pickle.load(file)
 
         predit = ''
+
+        ipk = Ipk
+        umur = Umur
+        semester = Semester
+        saudara = Saudara
 
         if Gender == 1:
             jekel = "Pria"
@@ -148,44 +120,23 @@ def main():
         else:
             pendap = '> 10.000.000'
 
-        if Domicile == 1:
-            Domisili = 'Dalam Kota'
-        elif Domicile == 2:
-            Domisili = 'Kota Sebelah'
-        elif Domicile == 3:
-            Domisili = 'Jarak Satu Kota'
-        elif Domicile == 4:
-            Domisili = 'Dalam Satu Pulau'
-        else : 
-            Domisili = 'Diluar Pulau' 
-
 
         if st.button('Prediksi Performa'):
             predit = model.predict(
-                [[Gender, Domicile, Economy, Campus_organization, 
-                  Total_login, N_access_forum, N_access_didactic_units, 
-                  Total_assignments, N_assignments_submitted, N_access_questionnaires, 
-                  N_attempts_questionnaires, N_answered_questions, 
-                  N_questionnaire_views, N_questionnaires_submitted, 
-                  N_reviews_questionnaire, Days_first_access_x, N_entries_course_x]]
+                [[Gender, Economy, Ipk]]
             )
             st.success (f"Hasil Prediksi : %.2f" % predit)
 
             #Simpan data
             df = pd.read_csv("Data_prediksi_pengguna.csv")
-            new_data = {"Nama":nama, "Status":status, "Jenis_Kelamin":jenkel, "Kota_Tinggal":kota_tinggal, "Jenis Kelamin":jekel, 
-                        "Ekonomi":pendap, "Domisili":Domisili, "Organisasi Kampus":Campus_organization,
-                        "Jumlah Login LMS":Total_login, "Jumlah Akses Forum":N_access_forum, "Jumlah Akses Materi":N_access_didactic_units, 
-                        "Jumlah Tugas":Total_assignments, "Jumlah Upload Tugas":N_assignments_submitted, "Jumlah Membuka Quiz":N_access_questionnaires,
-                        "Jumlah Melengkapi Quiz":N_attempts_questionnaires, "Jumlah Menjawab Quiz":N_answered_questions, 
-                        "Jumlah Melihat Quiz":N_questionnaire_views, "Jumlah Mengirim Quiz":N_questionnaires_submitted,    
-                        "Jumlah Ulasan Quiz":N_reviews_questionnaire,  "Minggu Keberapa Akses LMS":Days_first_access_x,   
-                        "Jumlah Masuk Ke Mata Kuliah":N_entries_course_x, "Hasil Prediksi":predit
+            new_data = {"Nama":nama, "Status":status, "Jenis Kelamin":jekel, "Umur":umur,
+                        "IPK":ipk, "Semester": semester,
+                        "Ekonomi":pendap, "Saudara":saudara, "Hasil Prediksi":predit
                         }
 
             df = df.append(new_data, ignore_index=True)
             df.to_csv("Data_prediksi_pengguna.csv", index=False)
-            v_data = df.iloc[:, 4:22]
+            v_data = df.iloc[:, 1:8]
             vlast= v_data.tail(1)
             st.dataframe(vlast.set_index(vlast.columns[0]))
             
@@ -194,24 +145,12 @@ def main():
         isi = ( "Prediksi Performa Akademik Mahasiswa Menggunakan Data Non-Akademik dan Akademik" + "\n" + "\n" +
                 "#Data Non Akademik" + "\n" +
                 "Gender = " + jekel + "\n" +
-                "Domisili = " + str(Domisili) + "\n" +
                 "Pendapatan Orang Tua = " + pendap + "\n" +
-                "Jumlah Organisasi Kampus Yang Diikuti = " + str(Campus_organization) + "\n" + "\n" +
+                "Jumlah Saudara = " + saudara + "\n" +
                 
-                "#Data Akademik (Kegiatan di LMS)" + "\n" +
-                "Jumlah Login LMS = " + str(Total_login) + "\n" +
-                "Jumlah Akses Forum LMS = " + str(N_access_forum) + "\n" +
-                "Jumlah Akses Materi = " + str(N_access_didactic_units) + "\n" +
-                "Jumlah Tugas = " + str(Total_assignments) + "\n" +
-                "Jumlah Upload Tugas = " + str(N_assignments_submitted) + "\n" +
-                "Jumlah Membuka Quiz = " + str(N_access_questionnaires) + "\n" +
-                "Jumlah Melengkapi Quiz = " + str(N_attempts_questionnaires) + "\n" +
-                "Jumlah Menjawab Quiz = " + str(N_answered_questions) + "\n" +
-                "Jumlah Melihat Quiz = " + str(N_questionnaire_views) + "\n" +
-                "Jumlah Mengirim Quiz = " + str(N_questionnaires_submitted) + "\n" +
-                "Jumlah Ulasan Quiz = " + str(N_reviews_questionnaire) + "\n" +
-                "Minggu Keberapa Akses LMS = " + str(Days_first_access_x) + "\n" +
-                "Jumlah Masuk Ke Mata Kuliah = " + str(N_entries_course_x) + "\n" + "\n" +
+                "#Data Akademik" + "\n" +
+                "IPK = " + str(Ipk) + "\n" +
+                "Prestasi = " + str(Prestasi) + "\n" +
                 
                 "#Hasil Prediksi adalah = " + str(predit)
                 )
@@ -222,12 +161,12 @@ def main():
     elif choice == "Hasil":
         st.subheader("Data Hasil Prediksi")
         df = pd.read_csv("Data_prediksi_pengguna.csv")
-        v_data = df.iloc[:, 4:22]
+        v_data = df.iloc[:, 1:8]
         st.dataframe(v_data.set_index(v_data.columns[0]))
         
     elif choice == "Penilaian":
         st.subheader("Nilai Aplikasi dan Saran")
-        st.write('Berikan penilaian terhadap sisterm informasi ini')
+        st.write('Berikan penilaian terhadap sistem ini')
 
         form= st.form("myform",clear_on_submit=True)
         col1,col2 = form.columns(2)
@@ -447,21 +386,21 @@ def main():
 
     elif choice == "Tentang":
         st.subheader("Tentang Aplikasi")
-        st.write('Sistem Informasi Prediksi Performa Akademik Mahasiswa ini dibuat dengan menggunakan bahasa pemprograman :blue[Python] dan :blue[Streamlit], Sisfo ini menggunakan model yang terbentuk dari algoritma :blue[Gradient Boosting Trees] yang telah dioptimasi hyperparameternya dengan menggunakan algoritma :blue[Gread Search]. Sedangkan data yang digunakan untuk membangun model berasal dari data akademik dan data non-akademikdemik yang diperoleh dari :blue[Universitas Muria Kudus].')
-        st.write('Dalam melakukan prediksi Model ini memiliki tingkat kesalahan sebesar :blue[37%], Adapun nilai bobot dari masing-masing fitur ditunjukkan pada halaman prediksi dan pada gambar dibawah ini.')
+        st.write('Sistem Informasi Prediksi Performa Akademik Mahasiswa ini dibuat dengan menggunakan bahasa pemprograman :blue[Python] dan :blue[Streamlit], sistem ini menggunakan model yang terbentuk dari algoritma :blue[LSTM dan SVM]. Sedangkan data yang digunakan untuk membangun model berasal dari data akademik dan data non-akademikdemik yang diperoleh dari :blue[Universitas Semarang].')
+        st.write('Dalam melakukan prediksi Model ini memiliki tingkat kesalahan sebesar 22%')
 
-        st.image(image, caption='Bobot Fitur')
+        #st.image(image, caption='Bobot Fitur')
 
-        st.write('Untuk informasi lebih lanjut terkait Prediksi Performa Akademik Mahasiswa ini, dapat menghubungi Email: :blue[arifin.m@umk.ac.id] ')
+        st.write('Untuk informasi lebih lanjut terkait Prediksi Performa Akademik Mahasiswa ini, dapat menghubungi Sri Handayani: 081232566827 ')
 
         txt_input= st.text_input('Masukkan Kode Download, Untuk Mendownload Hasil Prediksi', type="password")
 
         with open('PenilaianMasukan.csv','rb') as f:
-            if txt_input == "Ijin Sedot":    
+            if txt_input == "rahasia":    
                 st.download_button('Download Penilaian', f, file_name='PenilaianMasukan.csv', disabled=not txt_input)  # Defaults to 'text/plain'
 
         with open('Data_prediksi_pengguna.csv','rb') as f:
-           if txt_input == "Ijin Sedot": 
+           if txt_input == "rahasia": 
                 st.download_button('Download Hasil Prediksi', f, file_name='HasilPrediksi.csv')  # Defaults to 'text/plain'
 
 
